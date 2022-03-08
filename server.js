2cbfd58ff16447124ad31bda2b64244a590d1b56
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-03-08 01:57:10 -0600

    added index.js to apiRoutes and htmlRoutes folders, set up router.get functions for index.html and notes.html

---
+++ server.js
@@ -0,0 +1,16 @@
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+const path = require("path");
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+});
